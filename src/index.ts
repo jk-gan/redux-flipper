d6a815f033eceed7c0cf6077f5b4ee32b0f9786f
Gan Jun Kai <kuhn96@gmail.com>
2020-04-18 18:06:09 +0800

    feat: pass the time took for the action

--- src/index.ts
+++ src/index.ts
@@ -20,13 +20,15 @@ const registerPlugin = () => {
 
 const viewer = (store: any) => (next: any) => (action: { type: string }) => {
   let before = store.getState();
+  let startTime = Date.now();
   let result = next(action);
   if (currentConnection) {
     let after = store.getState();
     let now = Date.now();
     let state = {
-      id: now,
-      time: dayjs(now).format('HH:mm:ss.SSS'),
+      id: startTime,
+      time: dayjs(startTime).format('HH:mm:ss.SSS'),
+      took: `${(now - startTime).toFixed(2)} ms`,
       action,
       before,
       after,
