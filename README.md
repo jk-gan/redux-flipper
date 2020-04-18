468b83aca2b344f2b2cc4365f14d38413f52fb35
Gan Jun Kai <kuhn96@gmail.com>
2020-04-18 23:31:09 +0800

    BREAKING: redesign the connection

--- README.md
+++ README.md
@@ -16,23 +16,18 @@ cd ios && pod install
 2. Add the middleware into your redux store:
 ```javascript
 import { createStore, applyMiddleware } from 'redux';
-import { viewer } from 'redux-flipper';
 
-let store = createStore(RootReducer, {}, applyMiddleware(viewer));
+if (__DEV__) {
+  const { createViewer } = require('redux-flipper').default;
+
+  let viewer = createViewer();
+  let store = createStore(RootReducer, {}, applyMiddleware(viewer));
+} else {
+  let store = createStore(RootReducer, {});
+}
 ```
 
-3. Register plugin in your app:
-```javascript
-// Add this line in your root component
-import react, { useEffect } from 'react';
-import { registerPlugin } from 'redux-flipper';
+3. Install [flipper-plugin-redux-viewer](https://github.com/jk-gan/flipper-plugin-redux-viewer) in Flipper desktop client
 
-useEffect(() => {
-  registerPlugin();
-}, [])
-```
-
-4. Install [flipper-plugin-redux-viewer](https://github.com/jk-gan/flipper-plugin-redux-viewer) in Flipper desktop client
-
-5. Start your app, then you should be able to see Redux Viewer on your Flipper app
+4. Start your app, then you should be able to see Redux Viewer on your Flipper app
 
--- src/index.ts
+++ src/index.ts
@@ -6,61 +6,62 @@ const error = {
   NO_STORE: 'NO_STORE',
 };
 
-const registerPlugin = (store: any) => {
-  addPlugin({
-    getId() {
-      return 'flipper-plugin-redux-viewer';
-    },
-    onConnect(connection: any) {
-      currentConnection = connection;
-
-      currentConnection.receive(
-        'dispatchAction',
-        (data: any, responder: any) => {
-          console.log('flipper redux dispatch action data', data);
-          // respond with some data
-          if (store) {
-            store.dispatch({ type: data.type, ...data.payload });
-
-            responder.success({
-              ack: true,
-            });
-          } else {
-            responder.success({
-              error: error.NO_STORE,
-              message: 'store is not setup in flipper plugin',
-            });
-          }
+const createDebugger = () => {
+  return (store: any) => (next: any) => (action: { type: string }) => {
+    if (currentConnection == null) {
+      addPlugin({
+        getId() {
+          return 'flipper-plugin-redux-debugger';
         },
-      );
-    },
-    onDisconnect() {},
-    runInBackground() {
-      return true;
-    },
-  });
+        onConnect(connection: any) {
+          currentConnection = connection;
+
+          currentConnection.receive(
+            'dispatchAction',
+            (data: any, responder: any) => {
+              console.log('flipper redux dispatch action data', data);
+              // respond with some data
+              if (store) {
+                store.dispatch({ type: data.type, ...data.payload });
+
+                responder.success({
+                  ack: true,
+                });
+              } else {
+                responder.success({
+                  error: error.NO_STORE,
+                  message: 'store is not setup in flipper plugin',
+                });
+              }
+            },
+          );
+        },
+        onDisconnect() {},
+        runInBackground() {
+          return true;
+        },
+      });
+    }
+    let startTime = Date.now();
+    let before = store.getState();
+    let result = next(action);
+    if (currentConnection) {
+      let after = store.getState();
+      let now = Date.now();
+
+      let state = {
+        id: startTime,
+        time: dayjs(startTime).format('HH:mm:ss.SSS'),
+        took: `${now - startTime} ms`,
+        action,
+        before,
+        after,
+      };
+      currentConnection.send('actionDispatched', state);
+    }
+
+    return result;
+  };
 };
 
-const viewer = (store: any) => (next: any) => (action: { type: string }) => {
-  let startTime = Date.now();
-  let before = store.getState();
-  let result = next(action);
-  if (currentConnection) {
-    let after = store.getState();
-    let now = Date.now();
-
-    let state = {
-      id: startTime,
-      time: dayjs(startTime).format('HH:mm:ss.SSS'),
-      took: `${now - startTime} ms`,
-      action,
-      before,
-      after,
-    };
-    currentConnection.send('actionDispatched', state);
-  }
-
-  return result;
-};
-
-export { viewer, registerPlugin };
+export default createDebugger;
