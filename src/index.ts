890c0996f86dd9231b9fdcf2be5909a0b465e736
Wai Pai Lee <pailee.wai@gmail.com>
2020-04-18 23:42:14 +0800

    feat: add action receiver

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { addPlugin } from 'react-native-flipper';
 
 let currentConnection: any = null;
+const error = {
+  NO_STORE: 'NO_STORE',
+};
 
-const registerPlugin = () => {
+const registerPlugin = (store: any) => {
   addPlugin({
     getId() {
       return 'flipper-plugin-redux-viewer';
@@ -10,6 +13,26 @@ const registerPlugin = () => {
     onConnect(connection) {
       console.log('connected to flipper-plugin-redux-viewer');
       currentConnection = connection;
+
+      currentConnection.receive(
+        'dispatchAction',
+        (data: any, responder: any) => {
+          console.log('flipper redux dispatch action data', data);
+          // respond with some data
+          if (store) {
+            store.dispatch({ type: data.type, ...data.payload });
+
+            responder.success({
+              ack: true,
+            });
+          } else {
+            responder.success({
+              error: error.NO_STORE,
+              message: 'store is not setup in flipper plugin',
+            });
+          }
+        },
+      );
     },
     onDisconnect() {},
     runInBackground() {
